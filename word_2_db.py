79137240ec0441eaeab120bd55bb8e513a3bc4cb
Mr-Major-K <125085450+Mr-Major-K@users.noreply.github.com>
2024-03-30 23:07:25 +0100

    Add files via upload

--- word_2_db.py
+++ word_2_db.py
@@ -1,21 +1,40 @@
-from flask import Flask
+import json
 from models.word import db, Word, create_tables
 from sqlalchemy.exc import IntegrityError
+from flask import Flask
 
 app = Flask(__name__)
+
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///../database/words.db"
 
-words_list = ["Stern", "Stand", "Stamm", "Stein", "Staub", "Sturm", "Stadt",
-              "Stufe", "Stiel", "Blatt", "Blüte", "Block", "Blase", "Blick",
-              "Blech", "Blend", "Blitz", "Bleib", "Rasch", "Hasse"]
+
+def word_generator(file_path):
+    # Überprüfen, ob die Datei eine JSON-Datei ist
+    if file_path.endswith('.json'):
+        with open(file_path, 'r', encoding='utf-8') as file:
+            words_list = json.load(file)
+    # Angenommen, es ist eine Textdatei
+    else:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            words_list = file.readlines()
+
+    # Wörter filtern, die genau 5 Buchstaben lang sind
+    for word in words_list:
+        # Entfernen Sie eventuelle Leerzeichen am Anfang und am Ende der Zeile
+        word = word.strip()
+        if len(word) == 5:
+            yield word
 
 
-def words_2_db(words: list) -> None:
-    for word in words:
-        word = word.upper()
+def word_2_db(file_path):
+    # Wörter in die Datenbank einfügen
+    for word in word_generator(file_path):
         try:
-            new_word = Word(word)
+
+            new_word = Word(word=word)
             db.session.add(new_word)
+
+            # Änderungen in der Datenbank speichern
             db.session.commit()
         except IntegrityError:
             db.session.rollback()
@@ -23,7 +42,15 @@ def words_2_db(words: list) -> None:
 
 
 if __name__ == '__main__':
+    # Pfad zur JSON-Datei
+    # file_path = 'woerter/German-words-1600000-words-multilines.json'
+    # file_path = 'woerter/Adjektive/Adjektive.txt'
+    # file_path = 'woerter/Substantive/substantiv_singular_das.txt'
+    # file_path = 'woerter/Substantive/substantiv_singular_der.txt'
+    # file_path = 'woerter/Substantive/substantiv_singular_die.txt'
+    # file_path = 'woerter/Verben/Verben_regelmaesig.txt'
+    file_path = 'woerter/Verben/Verben_unregelmaeßig_Infinitiv.txt'
     with app.app_context():
         create_tables()
         db.init_app(app)
-        words_2_db(words_list)
+        word_2_db(file_path)
